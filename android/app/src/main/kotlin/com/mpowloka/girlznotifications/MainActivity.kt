5045d59111befa716faef8c797b8265316b3e8f7
mpowloka <michalpowloka@interia.pl>
2019-04-05 23:52:23 +0200

    Create notifications channel

--- android/app/src/main/kotlin/com/mpowloka/girlznotifications/MainActivity.kt
+++ android/app/src/main/kotlin/com/mpowloka/girlznotifications/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.mpowloka.girlznotifications
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequestBuilder
@@ -15,6 +18,13 @@ class MainActivity : FlutterActivity() {
         super.onCreate(savedInstanceState)
         GeneratedPluginRegistrant.registerWith(this)
 
+        createNotificationsChannel()
+
+        observeFlutterChannel()
+
+    }
+
+    private fun observeFlutterChannel() {
         MethodChannel(flutterView, FLUTTER_NOTIFICATIONS_CHANNEL).setMethodCallHandler { call, result ->
 
             when (call.method) {
@@ -24,7 +34,7 @@ class MainActivity : FlutterActivity() {
                     WorkManager.getInstance().enqueueUniquePeriodicWork(
                             WORKER_NAME,
                             ExistingPeriodicWorkPolicy.REPLACE,
-                            PeriodicWorkRequestBuilder<NotificationWorker>(1, TimeUnit.SECONDS)
+                            PeriodicWorkRequestBuilder<NotificationWorker>(12, TimeUnit.HOURS)
                                     .build()
                     )
 
@@ -39,11 +49,30 @@ class MainActivity : FlutterActivity() {
 
             }
         }
+    }
 
+    private fun createNotificationsChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val channel = NotificationChannel(
+                    CHANNEL_ID,
+                    ANDROID_NOTIFICATIONS_CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     companion object {
+
+        private const val CHANNEL_ID = "42"
+
         private const val FLUTTER_NOTIFICATIONS_CHANNEL = "notifications"
+
+        private const val ANDROID_NOTIFICATIONS_CHANNEL_NAME = "Gentle reminders"
+
         private const val WORKER_NAME = "WORKER_NAME"
     }
 
