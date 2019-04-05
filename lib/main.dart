4150e3f3f2aea82f52279b753d46e44aa7df32cd
mpowloka <michalpowloka@interia.pl>
2019-04-06 00:15:17 +0200

    Change icon and application colors

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,9 @@ import 'package:girlz_notifications/home.dart';
 
 void main() => runApp(MaterialApp(
       title: "Gentle reminder",
+      theme: ThemeData(
+        primaryColorDark: Color.fromARGB(255, 53, 180, 142),
+        primaryColor: Color.fromARGB(255, 98, 174, 116),
+      ),
       home: HomeScreen(),
     ));
