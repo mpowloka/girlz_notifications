90035ced2575cb21c92c4be6e2a2823cadcef5de
mpowloka <michalpowloka@interia.pl>
2019-04-06 00:29:26 +0200

    Add some messages for Kasia

--- android/app/src/main/kotlin/com/mpowloka/girlznotifications/NotificationWorker.kt
+++ android/app/src/main/kotlin/com/mpowloka/girlznotifications/NotificationWorker.kt
@@ -5,6 +5,7 @@ import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import java.util.*
 
 class NotificationWorker(
         context: Context, workerParams: WorkerParameters
@@ -15,9 +16,11 @@ class NotificationWorker(
         val channelId = inputData.getString(TARGET_CHANNEL_ID_KEY) ?: return Result.failure()
 
         val notification = NotificationCompat.Builder(applicationContext, channelId)
-                .setContentTitle("Hey beauty!")
+                .setContentTitle(NOTIFICATION_CONTENT_TITLE)
                 .setSmallIcon(R.drawable.ic_launcher)
-                .setContentText("You look stunning today!")
+                .setContentText(
+                        AVAILABLE_CONTENT_TEXTS[Random().nextInt(AVAILABLE_CONTENT_TEXTS.size)]
+                )
                 .build()
 
         NotificationManagerCompat.from(applicationContext).notify(
@@ -28,6 +31,29 @@ class NotificationWorker(
     }
 
     companion object {
+
+        private const val NOTIFICATION_CONTENT_TITLE = "Kasia..."
+
+        private val AVAILABLE_CONTENT_TEXTS = listOf<String>(
+                "Wyglądasz doooooobrzeeee!",
+                "LUBIĘ CIĘ!",
+                "Paweł nie ma racji.",
+                "Miłego dnia <3",
+                "♫♫♫ Ty jesteś RUDA! ♫♫♫",
+                "No cześć, Maleńka...",
+                "Robisz coś wieczorem?",
+                "Czy sprawdziłaś już dzisiejszy status WorldWarBot 2020?",
+                "Bądź dobra dla ludzi, a ludzie będą dobrzy dla Ciebie Kochanie!",
+                "Jeżeli jesteś smutna... To przestań <3",
+                "Cieszę się, że Cię poznałem.",
+                "Nie wiem ile takich wiadomości powinienem wymyśleć...",
+                "Zjedz dzisiaj ananasa...",
+                "Uśmiechnij się :)",
+                "Uśmiechnij się bardzo :))",
+                "Przecież nie mogłem się od razu przyznać jaką piszę apke, prawda?",
+                "Przygodzie powiedziałem, że poznaliśmy się na Woodstocku",
+                "Paulina Pawlak myśli, że spadłaś na mnie z okna"
+        )
 
         const val TARGET_CHANNEL_ID_KEY = "TARGET_CHANNEL_ID_KEY"
 
